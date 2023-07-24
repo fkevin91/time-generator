fe31a59e6ffcdfcd97cb0375e90173a9625501a6
kevin <k.fardeau123@gmail.com>
2023-07-24 16:05:45 +0200

    fix form account

--- components/Account.vue
+++ components/Account.vue
@@ -80,48 +80,50 @@ async function updateProfile() {
     <form class="form-widget" @submit.prevent="updateProfile">
       <Avatar v-model:path="avatar_path" @upload="updateProfile" />
       <div class="mb-6">
-        <label for="mail" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">mail utilisateur</label>
+        <label for="mail" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email de connexion</label>
         <input type="mail" id="mail" :value="user.email" disabled class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="mail" required>
       </div>
-      <div class="mb-6">
-        <label for="full_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-        <input type="full_name" id="full_name" v-model="full_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="full_name" required>
+      <div class="flex justify-between">
+        <div class="mb-6">
+          <label for="full_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom*</label>
+          <input type="full_name" id="full_name" v-model="full_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="full_name" required>
+        </div>
+        <div class="mb-6">
+          <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom*</label>
+          <input type="username" id="username" v-model="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required>
+        </div>
       </div>
       <div class="mb-6">
-        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
-        <input type="username" id="username" v-model="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required>
-      </div>
-      <div class="mb-6">
-        <label for="mail_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email enterprise</label>
+        <label for="mail_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email enterprise*</label>
         <input type="mail_enterprise" id="mail_enterprise" :value="mail_enterprise" disabled class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="mail_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="siret_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Siret enterprise*</label>
+        <input type="siret_enterprise" id="siret_enterprise" v-model="siret_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="siret_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="name_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom enterprise*</label>
+        <input type="name_enterprise" id="name_enterprise" v-model="name_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="adress_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse enterprise*</label>
+        <input type="adress_enterprise" id="adress_enterprise" v-model="adress_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="adress_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="cp_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code postale enterprise*</label>
+        <input type="cp_enterprise" id="cp_enterprise" v-model="cp_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="cp_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="city_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ville enterprise*</label>
+        <input type="city_enterprise" id="city_enterprise" v-model="city_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="city_enterprise" required>
+      </div>
+      <div class="mb-6">
+        <label for="tel_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">N° tel enterprise*</label>
+        <input type="tel_enterprise" id="tel_enterprise" v-model="tel_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="tel_enterprise" required>
       </div>
       <div class="mb-6">
         <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Site Web</label>
         <input type="website" id="website" v-model="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="URL web site">
-      </div>
-      <div class="mb-6">
-        <label for="siret_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Siret enterprise</label>
-        <input type="siret_enterprise" id="siret_enterprise" v-model="siret_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="siret_enterprise" required>
-      </div>
-      <div class="mb-6">
-        <label for="name_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom enterprise</label>
-        <input type="name_enterprise" id="name_enterprise" v-model="name_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name_enterprise" required>
-      </div>
-      <div class="mb-6">
-        <label for="adress_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse enterprise</label>
-        <input type="adress_enterprise" id="adress_enterprise" v-model="adress_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="adress_enterprise" required>
-      </div>
-      <div class="mb-6">
-        <label for="cp_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code postale enterprise</label>
-        <input type="cp_enterprise" id="cp_enterprise" v-model="cp_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="cp_enterprise" required>
-      </div>
-      <div class="mb-6">
-        <label for="city_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ville enterprise</label>
-        <input type="city_enterprise" id="city_enterprise" v-model="city_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="city_enterprise" required>
-      </div>
-      <div class="mb-6">
-        <label for="tel_enterprise" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">N° tel enterprise</label>
-        <input type="tel_enterprise" id="tel_enterprise" v-model="tel_enterprise" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="tel_enterprise" required>
       </div>
       <div>
         <input
