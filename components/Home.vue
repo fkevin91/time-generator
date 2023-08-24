6198cc5285afc02680438d15a4fc273d25beec7e
kevin <k.fardeau123@gmail.com>
2023-08-24 16:19:25 +0200

    update button back to home

--- components/Home.vue
+++ components/Home.vue
@@ -20,8 +20,8 @@ const titlePage = ref('')
         <div class="rounded-lg shadow-xl p-3 bg-slate-200 w-60 text-center line-through" @click="menu = 'listinvoices'">Mes Facture</div>
       -->
     </div>
-    <div v-if="menu" @click="menu = ''" class="flex space-x-3 text-white mb-4 w-full">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <div v-if="menu" class="flex space-x-3 text-white mb-4 w-full">
+      <svg @click="menu = ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       <p>{{'Retour - ' + titlePage}}</p>
